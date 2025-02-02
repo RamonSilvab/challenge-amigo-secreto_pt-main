891a9f10e17f8613bdea64aaf6b8b467dedb217a
Ramon Silva <ramonsilvab05@gmail.com>
2025-02-02 20:26:38 -0300

    finalizando o desafio do amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomesAdicionados = [];
+let campo = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+
+function adicionarAmigo() {
+    let nomeParaAdicionar = document.getElementById('amigo').value;
+    
+    if (listaDeNomesAdicionados.includes(nomeParaAdicionar)) {
+        alert('Esse nome já está na lista!');
+    } else if (nomeParaAdicionar == '') {
+        alert('Digite um nome válido por favor!');
+    } else {
+        listaDeNomesAdicionados.push(nomeParaAdicionar);
+        exibeTextoNaTela();
+        limparCampo();
+    } 
+}
+
+function limparCampo() {
+    let campoAmigoAdicionado = document.getElementById('amigo');
+    campoAmigoAdicionado.value = '';
+}
+
+function exibeTextoNaTela() {
+    campo.innerHTML = ""; 
+
+    let contador = 0;
+    while (contador < listaDeNomesAdicionados.length) {
+        campo.innerHTML += `<li>${listaDeNomesAdicionados[contador]}</li>`;
+        contador++;
+    }
+}
+
+function sortearAmigo() {
+    if (listaDeNomesAdicionados.length === 0) {
+        alert('Adicione pelo menos um nome antes de sortear!');
+        return;
+    }
+    
+    let indiceSorteado = Math.round(Math.random() * (listaDeNomesAdicionados.length - 1));
+    let nomeSorteado = listaDeNomesAdicionados[indiceSorteado];
+
+    resultado.innerText = `O amigo secreto sorteado é: ${nomeSorteado}`;
+    campo.innerHTML = "";
+    listaDeNomesAdicionados = [];
+}
